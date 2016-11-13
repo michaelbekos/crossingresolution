68edd8b7bce0297c0ef100a806b541d7d0b82fef
plneappl <wegsim@sl.supra-net.net>
2016-11-13 16:40:35 +0100

    Cleanup List/forEach --> stream/collect, other

--- src/layout/algo/MinimumAngleImprovement.java
+++ src/layout/algo/MinimumAngleImprovement.java
@@ -10,7 +10,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.stream.Collectors;
 /**
  * Created by Jessica Wolz on 06.11.16.
  */
@@ -39,23 +39,23 @@ public class MinimumAngleImprovement {
         //    e.printStackTrace();
         //}
         //List<PointD> temp = func.readFromFile(name);
-        List<PointD> origNodeLocations = new ArrayList<PointD>(graph.getNodes().size());
-        graph.getNodes().forEach(
-            u -> origNodeLocations.add(
-                new PointD(
-                    u.getLayout().getCenter().getX(), 
-                    u.getLayout().getCenter().getY())));
-        ArrayList<Double> minAngles = new ArrayList<>();
+        List<PointD> origNodeLocations = 
+            graph.getNodes().stream()
+                .map(u -> new PointD(
+                        u.getLayout().getCenter().getX(), 
+                        u.getLayout().getCenter().getY()))
+                .collect(Collectors.toList());
+        List<Double> minAngles = new ArrayList<>();
 
         for(int j = 0; j * x < 1; j++){
-
+            PointD offset = new PointD(x * j, 0);
             //moving to right by x steps
             for (INode u : tempGraph.getNodes()) {
-                double u_x = u.getLayout().getCenter().getX()+(x * j);
-                double u_y = u.getLayout().getCenter().getY();
-                tempGraph.setNodeCenter(u, new PointD(u_x, u_y));
-                tempGraph = GridDrawing.roundingGridTemp(tempGraph);
+                tempGraph.setNodeCenter(u,
+                    PointD.add(u.getLayout().getCenter(), offset));
             }
+            
+            tempGraph = GridDrawing.roundingGridTemp(tempGraph);
 
             minAngles.add(MinimumAngle.getMinimumAngle(tempGraph).get());
 
@@ -67,10 +67,7 @@ public class MinimumAngleImprovement {
                 tempGraph.setNodeCenter(u, new PointD(u_x, u_y));
                 i++;
             }
-
         }
-
-        //minAngles.add(MinimumAngle.getMinimumAngle(tempGraph).get());
 
 
         for(int j = 0; j < minAngles.size(); j++){
