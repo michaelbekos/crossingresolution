22c138eac84d7be891a22da184d6a9d7585fbb21
plneappl <wegsim@sl.supra-net.net>
2016-11-19 21:58:50 +0100

    degree added to IncidentEdgesForce

--- src/layout/algo/IncidentEdgesForce.java
+++ src/layout/algo/IncidentEdgesForce.java
@@ -1,16 +1,15 @@
 package layout.algo;
 
-
 import util.*;
 import java.util.function.Function;
 import com.yworks.yfiles.geometry.*;
 
 public class IncidentEdgesForce extends ForceAlgorithm {
-  Function<PointD, Function<PointD, Function<Double, Tuple2<PointD, PointD>>>> f;
-  public IncidentEdgesForce(Function<PointD, Function<PointD, Function<Double, Tuple2<PointD, PointD>>>> f1){
+  Function<PointD, Function<PointD, Function<Double, Function<Int, Tuple2<PointD, PointD>>>>> f;
+  public IncidentEdgesForce(Function<PointD, Function<PointD, Function<Double, Function<Int, Tuple2<PointD, PointD>>>>> f1){
     f = f1;
   }
-  public Function<PointD, Function<Double, Tuple2<PointD, PointD>>> apply(PointD t){
+  public Function<PointD, Function<Double, Function<Int, Tuple2<PointD, PointD>>>> apply(PointD t){
     return f.apply(t);
   }
 }
