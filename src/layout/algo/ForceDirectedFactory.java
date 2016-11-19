28ebe2467d6db73f29ed65e42ff811deabf162d7
Jessica Wolz <jessica.wolz@student.uni-tuebingen.de>
2016-11-19 19:06:14 +0100

    Adding Cosine Force for Crossing Angles

--- src/layout/algo/ForceDirectedFactory.java
+++ src/layout/algo/ForceDirectedFactory.java
@@ -1,5 +1,6 @@
 package layout.algo;
 
+import algorithms.graphs.MinimumAngle;
 import com.yworks.yfiles.algorithms.GraphConnectivity;
 import com.yworks.yfiles.algorithms.YPoint;
 import com.yworks.yfiles.algorithms.YVector;
@@ -7,9 +8,13 @@ import com.yworks.yfiles.graph.IGraph;
 import com.yworks.yfiles.graph.IMapper;
 import com.yworks.yfiles.graph.INode;
 import com.yworks.yfiles.layout.YGraphAdapter;
+import util.Tuple3;
+import util.graph2d.Intersection;
+import util.graph2d.LineSegment;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
 /**
  * Created by bekos on 10/28/16.
@@ -100,4 +105,55 @@ public class ForceDirectedFactory {
             map.getValue(u).addAll(vectors);
         }
     }
+
+    /**
+     * Calculate cosine forces
+     * @param graph - the input graph.
+     * @param threshold - a threshold value.
+     * @param map - the NodeMap, where the calculated forces will be stored (might be non-empty).
+     */
+    public static void calculateCosineForcesEades(IGraph graph, double threshold, IMapper<INode, List<YVector>> map)
+    {
+        List<Tuple3<LineSegment, LineSegment, Intersection>> crossings = MinimumAngle.getCrossings(graph);
+        List<YVector> vectors;
+
+        for (Tuple3<LineSegment, LineSegment, Intersection> c : MinimumAngle.getCrossings(graph))
+        {
+            // crossing of (a,b) and (c,d)
+            double a_y = c.a.p1.getY();
+            double a_x = c.a.p1.getX();
+
+            double b_x = c.a.p2.getX();
+            double b_y = c.a.p2.getY();
+
+            double c_y = c.b.p1.getY();
+            double c_x = c.b.p1.getX();
+
+            double d_x = c.b.p2.getX();
+            double d_y = c.b.p2.getY();
+
+            YPoint p_a = new YPoint(a_x, a_y);
+            YPoint p_b = new YPoint(b_x, b_y);
+            YPoint p_c = new YPoint(a_x, a_y);
+            YPoint p_d = new YPoint(b_x, b_y);
+
+            YVector firstTemp = new YVector(p_a, p_b);
+            YVector secTemp = new YVector(p_c,p_d);
+
+            firstTemp.norm();
+            secTemp.norm();
+            firstTemp.scale(threshold * Math.cos(c.c.angle));
+            secTemp.scale(threshold * Math.cos(c.c.angle));
+
+            map.getValue(c.a.n1.get()).add(firstTemp);
+            map.getValue(c.a.n2.get()).add(firstTemp);
+            map.getValue(c.b.n1.get()).add(secTemp);
+            map.getValue(c.b.n2.get()).add(secTemp);
+
+
+        }
+
+
+    }
+
 }
