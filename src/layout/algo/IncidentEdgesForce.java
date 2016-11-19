9282c7ef1742d0f302af60c44099794cf0c9526d
plneappl <wegsim@sl.supra-net.net>
2016-11-19 22:04:09 +0100

    whoops^2

--- src/layout/algo/IncidentEdgesForce.java
+++ src/layout/algo/IncidentEdgesForce.java
@@ -5,11 +5,11 @@ import java.util.function.Function;
 import com.yworks.yfiles.geometry.*;
 
 public class IncidentEdgesForce extends ForceAlgorithm {
-  Function<PointD, Function<PointD, Function<Double, Function<Int, Tuple2<PointD, PointD>>>>> f;
-  public IncidentEdgesForce(Function<PointD, Function<PointD, Function<Double, Function<Int, Tuple2<PointD, PointD>>>>> f1){
+  Function<PointD, Function<PointD, Function<Double, Function<Integer, Tuple2<PointD, PointD>>>>> f;
+  public IncidentEdgesForce(Function<PointD, Function<PointD, Function<Double, Function<Integer, Tuple2<PointD, PointD>>>>> f1){
     f = f1;
   }
-  public Function<PointD, Function<Double, Function<Int, Tuple2<PointD, PointD>>>> apply(PointD t){
+  public Function<PointD, Function<Double, Function<Integer, Tuple2<PointD, PointD>>>> apply(PointD t){
     return f.apply(t);
   }
 }
