c6c1fb24122e6d2b77ef242d16da574112214237
plneappl <wegsim@sl.supra-net.net>
2016-11-30 19:34:33 +0100

    Implemented weighting by crossing angle and node degree. Closes #7

--- src/util/graph2d/Intersection.java
+++ src/util/graph2d/Intersection.java
@@ -2,7 +2,7 @@ package util.graph2d;
 
 import com.yworks.yfiles.geometry.PointD;
 
-public class Intersection{
+public class Intersection implements Comparable<Intersection> {
   public PointD intersectionPoint;
   public Double angle, orientedAngle;
   @Override 
@@ -25,5 +25,9 @@ public class Intersection{
         ((Intersection) o).angle.equals(angle);
     }
     return false;
+  }
+  @Override
+  public int compareTo(Intersection o){
+    return orientedAngle.compareTo(o.orientedAngle);
   }
 }
