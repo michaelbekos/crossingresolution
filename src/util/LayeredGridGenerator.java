08d25cf68cc9a888c23fdd50ca194af1e80750f6
plneappl <wegsim@sl.supra-net.net>
2017-02-03 18:32:27 +0100

    *fixes*

--- src/util/LayeredGridGenerator.java
+++ src/util/LayeredGridGenerator.java
@@ -10,32 +10,36 @@ import com.yworks.yfiles.graph.INode;
 public class LayeredGridGenerator {
 
     public static void generate(IGraph g, int xCount, int yCount, int layers){
+        int offset = 100;
+        int o = offset;
+        int gridOffset = offset / layers;
+        int go = gridOffset;
         g.clear();
-        layers=2;
-        INode[][] nodes = new INode[xCount*layers][];
-        for(int l = 1; l <= layers; l++) {
+        INode[][][] nodes = new INode[layers][][];
+        for(int l = 0; l < layers; l++) {
+            nodes[l] = new INode[xCount][];
             // create nodes in x & y dimension
             for (int x = 0; x < xCount; x++) {
-                nodes[x*l] = new INode[yCount];
+                nodes[l][x] = new INode[yCount];
                 for (int y = 0; y < yCount; y++) {
-                    PointD pos = new PointD(500 + 50 * x - 25 * l, 500 + 50 * y + 25 * l);
+                    PointD pos = new PointD(500 + o * x + go * l, 500 + o * y - go * l);
                     INode n = g.createNode(pos);
-                    nodes[x*l][y] = n;
+                    nodes[l][x][y] = n;
                 }
             }
             // create edges in x dimension
             for (int x = 0; x < xCount - 1; x++) {
                 for (int y = 0; y < yCount; y++) {
-                    INode source = nodes[x*l][y],
-                            target = nodes[(x + 1)*l][y];
+                    INode source = nodes[l][x][y],
+                            target = nodes[l][x+1][y];
                     g.createEdge(source, target);
                 }
             }
             // create edges in y dimension
             for (int x = 0; x < xCount; x++) {
                 for (int y = 0; y < yCount - 1; y++) {
-                    INode source = nodes[x*l][y],
-                            target = nodes[x*l][(y + 1)];
+                    INode source = nodes[l][x][y],
+                            target = nodes[l][x][y+1];
                     g.createEdge(source, target);
                 }
             }
@@ -45,18 +49,17 @@ public class LayeredGridGenerator {
             System.out.println(n + " " + n.getLayout().getCenter());
         }
 
-        for(int l = 1; l < layers; l++) {
-            if (layers > 1) {
+        for(int l = 0; l < layers - 1; l++) {
                 // create edges between layers
                 for (int x = 0; x < xCount; x++) {
                     for (int y = 0; y < yCount; y++) {
-                        INode source = nodes[x * l][y],
-                                target = nodes[x * (l + 1)][y];
+                        INode source = nodes[l][x][y],
+                                target = nodes[l+1][x][y];
                         g.createEdge(source, target);
 
                     }
                 }
-            }
+            
         }
 
     }
