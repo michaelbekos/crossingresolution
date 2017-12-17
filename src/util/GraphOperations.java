8ca0e11cb4aae682ff0aced060a92d853682feb1
cgeckeler <christiangeckeler@gmail.com>
2017-12-17 11:46:11 +0100

    Added node scaling inversely proportional to zoom (fixes issue with scaling, scale me to box, etc)

--- src/util/GraphOperations.java
+++ src/util/GraphOperations.java
@@ -175,7 +175,6 @@ public class GraphOperations {
         }    	
         for(INode u : g.getNodes()){
             nodePose.setValue(u, new PointD((u.getLayout().getCenter().getX()-minX) * scaleValue, (u.getLayout().getCenter().getY()-minY) * scaleValue));
-//            g.setNodeLayout(u, new RectD(u.getLayout().getX()*scaleValue,u.getLayout().getY()*scaleValue,u.getLayout().getWidth()*scaleValue,u.getLayout().getHeight()*scaleValue));
             g.setNodeLayout(u, new RectD(u.getLayout().getX(),u.getLayout().getY(),u.getLayout().getWidth(),u.getLayout().getHeight()));
         }
         return nodePose;
