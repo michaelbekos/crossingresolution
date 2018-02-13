1a2c007fbe081a1721b0310da027cfb55cb47791
Lukas Holländer <jan-lukas.hollaender@student-uni-tuebingen.de>
2018-02-13 21:10:25 +0100

    fix various terminal scripts (untested...)

--- src/scripts/run.sh
+++ src/scripts/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-env DISPLAY=:0 java -cp "../yfiles-for-java.jar:../commons-cli-1.4.jar:../" main/MainFrame
+env DISPLAY=:0 java -cp "../yfiles-for-java.jar:../commons-cli-1.4.jar:../" main/Main
