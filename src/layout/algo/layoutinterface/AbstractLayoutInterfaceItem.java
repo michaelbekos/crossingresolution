2c147c15c3aacfe0ffdb1e34df518212ff35b659
Lukas Holländer <jan-lukas.hollaender@student-uni-tuebingen.de>
2018-02-26 16:04:31 +0100

    #129: documentation of the layoutinterface package

--- src/layout/algo/layoutinterface/AbstractLayoutInterfaceItem.java
+++ src/layout/algo/layoutinterface/AbstractLayoutInterfaceItem.java
@@ -1,5 +1,11 @@
 package layout.algo.layoutinterface;
 
+/**
+ * A very generic class representing an abstract two-way parameter for algorithms. Values can be both set and retrieved
+ * by both the algorithm and the caller-side. Setting and getting the value is thread-safe.
+ *
+ * @param <T> The type of the value that will be stored in this parameter.
+ */
 public abstract class AbstractLayoutInterfaceItem<T> {
   private volatile T value;
   private final String name;
