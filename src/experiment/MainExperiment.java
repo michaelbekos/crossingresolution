f0d00791e4e1251f6fc514e62e8d6c2129d39432
Amssen <amadaeus.spallek@student.uni-tuebingen.de>
2018-06-05 13:25:30 +0200

    improved construction (MainExperiment), addded organic to openSimpleFiles

--- src/experiment/MainExperiment.java
+++ src/experiment/MainExperiment.java
@@ -5,135 +5,93 @@ package experiment;
  */
 
 public class MainExperiment {
-    static String inputDir = "E:\\graph\\rome\\rome\\";
-    static String outputDir = "E:\\graph\\results\\rome_Angular_01062018\\";
+    static String inputRomeDir = "E:\\graph\\rome\\rome\\";
+    static String inputNorthDir = "E:\\graph\\north\\north\\north\\";
+
+    //static String inputRomeDir = "E:\\graph\\rome\\smallSet\\"; // testing
+   //  static String inputNorthDir = "E:\\graph\\rome\\smallSet\\"; // testing
+
+    static String outputDir = "E:\\graph\\results\\05_06_2018\\";
+
+    static  Experiments e;
+
+    /** experiments configurations **/
+    static long maxCalcTime = Long.MAX_VALUE;
+    static int numOfIterationPerStep = 100;
+    static int numOfSteps = 1000;
+    static int boxSize = 10000;
+    static boolean planarGraphsAllowed = false;
+    static boolean unconnectedGraphsAllowed = false;
+    static boolean useCrossingRes;  // if crossing and angular is enabled, the experiment focus on total resolution
+    static boolean useAngularRes;
+    static boolean useAspectRatio;
+
+    /** run booleans  **/
+    static boolean runRome = true;
+    static boolean runNorth = false;
+    static boolean runRandomMovement = false;
+    static boolean runRandomMovementWithAspectRatio = true;
+    static boolean runForce = false;
+    static boolean runTotalForce = false;
+
+
 
     public static void main(String[] args) {
-        //Experiments e = new Experiments();
 
-        // Experiments e = new Experiments(("E:\\graph\\graphml\\"), "E:\\graph\\afterRandom\\graphml2\\");
-
-
-
-        //rome
-
-     inputDir = "E:\\graph\\rome\\rome\\";
-     outputDir = "E:\\graph\\results\\rome_Crossing_aspectratio_of_5_05062018\\";
-
-      Experiments e = new Experiments(inputDir, outputDir,
-              Long.MAX_VALUE, 100, 1000, 10000,
-              false, false, false, true);
-       e.runOnlyRandom();
-
-       e = new Experiments(inputDir, outputDir,
-               Long.MAX_VALUE, 100, 1000, 10000,
-               false, false, false, true);
-        //e.runOnlyForce();
-
-        e = new Experiments(inputDir, outputDir,
-                Long.MAX_VALUE, 100, 1000, 10000,
-                false, false, false, true);
-        //e.runOnlyTotalResolutionForce();
-
-     inputDir = "E:\\graph\\rome\\rome\\";
-     outputDir = "E:\\graph\\results\\rome_Angular_aspectratio_of_5_04062018\\";
-
-    e = new Experiments(inputDir, outputDir,
-             Long.MAX_VALUE, 100, 1000, 10000,
-             false, false, false, true);
-     //   e.runOnlyRandom();
-
-     e = new Experiments(inputDir, outputDir,
-             Long.MAX_VALUE, 100, 1000, 10000,
-             false, false, false, true);
-       //e.runOnlyForce();
-
-     e = new Experiments(inputDir, outputDir,
-             Long.MAX_VALUE, 100, 1000, 10000,
-             false, false, false, true);
-     //e.runOnlyTotalResolutionForce();
+        /** crossing resolution (CR)**/
+        useCrossingRes = true;
+        useAngularRes = false;
+        run("CR\\");
+        /** angular resolution (AR)**/
+        useCrossingRes = false;
+        useAngularRes = true;
+        run("AR\\");
+        /** total resolution (TR) **/
+        useCrossingRes = true;
+        useAngularRes = true;
+        run("TR\\");
 
 
-     inputDir = "E:\\graph\\rome\\rome\\";
-     outputDir = "E:\\graph\\results\\rome_Total_aspectratio_of_5_04062018\\";
+    }
+    public static void run(String mode){
+        if(runRome){
+            useAspectRatio = false;
+            e = new Experiments(inputRomeDir, outputDir + "rome\\"+ mode,
+                    maxCalcTime, numOfIterationPerStep, numOfSteps, boxSize,
+                    planarGraphsAllowed, unconnectedGraphsAllowed, useCrossingRes, useAngularRes, useAspectRatio);
+            if(runRandomMovement)
+                e.runOnlyRandom();  //run random without aspect ratio
+            if(runForce)
+                e.runOnlyForce();
+            if(runTotalForce)
+                e.runOnlyTotalResolutionForce();
+            if(runRandomMovementWithAspectRatio){
+                useAspectRatio = true;
+                e = new Experiments(inputRomeDir, outputDir + "rome\\"+ mode,
+                        maxCalcTime, numOfIterationPerStep, numOfSteps, boxSize,
+                        planarGraphsAllowed, unconnectedGraphsAllowed, useCrossingRes, useAngularRes, useAspectRatio);
+                e.runOnlyRandom(); //run random  movement with aspect ratio
+            }
 
-     e = new Experiments(inputDir, outputDir,
-             Long.MAX_VALUE, 100, 1000, 10000,
-             false, false, false, true);
-        e.runOnlyRandom();
-
-     e = new Experiments(inputDir, outputDir,
-             Long.MAX_VALUE, 100, 1000, 10000,
-             false, false, false, true);
-     //  e.runOnlyForce();
-
-     e = new Experiments(inputDir, outputDir,
-             Long.MAX_VALUE, 100, 1000, 10000,
-             false, false, false, true);
-     //e.runOnlyTotalResolutionForce();
-
-
-
-     //north
-/*
-
-         inputDir = "E:\\graph\\north\\north\\north\\";
-         outputDir = "E:\\graph\\results\\north_Crossing_01062018\\";
-
-        e = new Experiments(inputDir, outputDir,
-                Long.MAX_VALUE, 100, 1000, 10000,
-                false, false, true, false);
-        e.runOnlyRandom();
-
-        e = new Experiments(inputDir, outputDir,
-                Long.MAX_VALUE, 100, 1000, 10000,
-                false, false, true, false);
-        e.runOnlyForce();
-
-        e = new Experiments(inputDir, outputDir,
-                Long.MAX_VALUE, 100, 1000, 10000,
-                false, false, true, false);
-        e.runOnlyTotalResolutionForce();
-
-        inputDir = "E:\\graph\\north\\north\\north\\";
-        outputDir = "E:\\graph\\results\\north_Angular_01062018\\";
-
-        e = new Experiments(inputDir, outputDir,
-                Long.MAX_VALUE, 100, 1000, 10000,
-                false, false, false, true);
-        e.runOnlyRandom();
-
-        e = new Experiments(inputDir, outputDir,
-                Long.MAX_VALUE, 100, 1000, 10000,
-                false, false, false, true);
-        e.runOnlyForce();
-
-        e = new Experiments(inputDir, outputDir,
-                Long.MAX_VALUE, 100, 1000, 10000,
-                false, false, false, true);
-        e.runOnlyTotalResolutionForce();
-
-
-
-        inputDir = "E:\\graph\\north\\north\\north\\";
-        outputDir = "E:\\graph\\results\\north_Total_01062018\\";
-
-        e = new Experiments(inputDir, outputDir,
-                Long.MAX_VALUE, 100, 1000, 10000,
-                false, false, true, true);
-        e.runOnlyRandom();
-
-        e = new Experiments(inputDir, outputDir,
-                Long.MAX_VALUE, 100, 1000, 10000,
-                false, false, true, true);
-        e.runOnlyForce();
-
-        e = new Experiments(inputDir, outputDir,
-                Long.MAX_VALUE, 100, 1000, 10000,
-                false, false, true, true);
-        e.runOnlyTotalResolutionForce();
-
-
-*/
+        }
+        if(runNorth){
+            useAspectRatio = false;
+            e = new Experiments(inputNorthDir, outputDir + "north\\"+ mode,
+                    maxCalcTime, numOfIterationPerStep, numOfSteps, boxSize,
+                    planarGraphsAllowed, unconnectedGraphsAllowed, useCrossingRes, useAngularRes, useAspectRatio);
+            if(runRandomMovement)
+                e.runOnlyRandom();  //run random without aspect ratio
+            if(runForce)
+                e.runOnlyForce();
+            if(runTotalForce)
+                e.runOnlyTotalResolutionForce();
+            if(runRandomMovementWithAspectRatio){
+                useAspectRatio = true;
+                e = new Experiments(inputNorthDir, outputDir + "north\\"+ mode,
+                        maxCalcTime, numOfIterationPerStep, numOfSteps, boxSize,
+                        planarGraphsAllowed, unconnectedGraphsAllowed, useCrossingRes, useAngularRes, useAspectRatio);
+                e.runOnlyRandom(); //run random  movement with aspect ratio
+            }
+        }
     }
 }
